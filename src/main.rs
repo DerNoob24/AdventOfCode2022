a279ad67fa3478f8db94495e69940bf12f08331e
Dirk Kremer <dirk.kremer@stadlerrail.com>
2022-12-04 15:37:25 +0100

    Day04

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod days {
     pub mod day01;
     pub mod day02;
     pub mod day03;
+    pub mod day04;
 }
 
 fn main() {
     days::day01::tasks();
     days::day02::tasks();
     days::day03::tasks();
+    days::day04::tasks();
 }
